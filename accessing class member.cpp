797f2e9ff02b26fc31f48a5e6af12ef259d51955
CHANCHALPATIDAR123 <169046773+CHANCHALPATIDAR123@users.noreply.github.com>
2024-08-06 15:09:10 +0530

    Add files via upload

---
+++ accessing class member.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class student
+{
+	private:
+		char name[20];
+		int roll;
+	public:
+	            void getdata();
+	            void display(){
+	            	cout<<"Name:"<<name<<endl;
+	            	cout<<"Roll:"<<roll<<endl;
+	            }	
+};
+void student::getdata(void){
+	cout<<"Enter Name:";
+	cin>>name;
+	cout<<"Enter Roll:";
+	cin>>roll;
+}
+int main(){
+	student a;
+	a.getdata();
+               a.display();
+}
